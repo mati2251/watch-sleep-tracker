753bfb206cb138315d36a026406ba4c155bee00a
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2024-01-02 13:49:57 +0100

    improved scan

--- dev/database/scan.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from bleak import discover
-
-async def scan_for_devices():
-    devices = await discover()
-    for d in devices:
-        print(f"Device: {d.name}, Address: {d.address}, RSSI: {d.rssi}")
-
-asyncio.run(scan_for_devices())
